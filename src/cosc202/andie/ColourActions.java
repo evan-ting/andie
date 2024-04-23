e5a3abdee0f6db875d987df6e7aa9c209134c958
Evan Ting <159759886+evan-ting@users.noreply.github.com>
2024-04-23 22:47:38 +1200

    Initial commit, add project skeleton files
    This was the initial state of the project that was handed over to my
    team, and we built upon it by adding new features to the application.

---
+++ src/cosc202/andie/ColourActions.java
@@ -0,0 +1,100 @@
+package cosc202.andie;
+
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * <p>
+ * Actions provided by the Colour menu.
+ * </p>
+ * 
+ * <p>
+ * The Colour menu contains actions that affect the colour of each pixel directly 
+ * without reference to the rest of the image.
+ * This includes conversion to greyscale in the sample code, but more operations will need to be added.
+ * </p>
+ * 
+ * <p> 
+ * <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>
+ * </p>
+ * 
+ * @author Steven Mills
+ * @version 1.0
+ */
+public class ColourActions {
+    
+    /** A list of actions for the Colour menu. */
+    protected ArrayList<Action> actions;
+
+    /**
+     * <p>
+     * Create a set of Colour menu actions.
+     * </p>
+     */
+    public ColourActions() {
+        actions = new ArrayList<Action>();
+        actions.add(new ConvertToGreyAction("Greyscale", null, "Convert to greyscale", Integer.valueOf(KeyEvent.VK_G)));
+    }
+
+    /**
+     * <p>
+     * Create a menu containing the list of Colour actions.
+     * </p>
+     * 
+     * @return The colour menu UI element.
+     */
+    public JMenu createMenu() {
+        JMenu fileMenu = new JMenu("Colour");
+
+        for(Action action: actions) {
+            fileMenu.add(new JMenuItem(action));
+        }
+
+        return fileMenu;
+    }
+
+    /**
+     * <p>
+     * Action to convert an image to greyscale.
+     * </p>
+     * 
+     * @see ConvertToGrey
+     */
+    public class ConvertToGreyAction extends ImageAction {
+
+        /**
+         * <p>
+         * Create a new convert-to-grey action.
+         * </p>
+         * 
+         * @param name The name of the action (ignored if null).
+         * @param icon An icon to use to represent the action (ignored if null).
+         * @param desc A brief description of the action  (ignored if null).
+         * @param mnemonic A mnemonic key to use as a shortcut  (ignored if null).
+         */
+        ConvertToGreyAction(String name, ImageIcon icon, String desc, Integer mnemonic) {
+            super(name, icon, desc, mnemonic);
+        }
+
+        /**
+         * <p>
+         * Callback for when the convert-to-grey action is triggered.
+         * </p>
+         * 
+         * <p>
+         * This method is called whenever the ConvertToGreyAction is triggered.
+         * It changes the image to greyscale.
+         * </p>
+         * 
+         * @param e The event triggering this callback.
+         */
+        public void actionPerformed(ActionEvent e) {
+            target.getImage().apply(new ConvertToGrey());
+            target.repaint();
+            target.getParent().revalidate();
+        }
+
+    }
+
+}
